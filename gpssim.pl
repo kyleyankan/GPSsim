8d125b702ee5ce51537951a2f0db22d073759ccc
KyleYankan <KyleYankanich@gmail.com>
2013-09-16 18:20:44 -0400

    Made random paths, easier to modify, etc

--- gpssim.pl
+++ gpssim.pl
@@ -6,19 +6,32 @@ use GPS::Point;
 
 $point = GPS::Point->new(lat=> 39.9507, lon=> -75.1347, speed => 0);
 
-print scalar($point->latlon), "\n";
+print scalar($point->lat), "," , scalar($point->long), "\n";
 
 
-while (1) {
-	$time = 60;
-	$direction = 180;
+foreach my $i (0..1000) {
+	$time = 5.0; #in seconds
+	$direction = 180+ rand(45); #moving south then westward
 	$speed = 22; #knots - 1,852 meters in a nautical mile
 
 	#above is where I'd get the time, speed, and direction
-	$distance = ($speed*1852) / $time;
+	$distance = (($speed*1852) / (60*60)* $time);
 
 	$point = $point->forward($distance =>$direction); 
+	print scalar($point->lat), "," , scalar($point->long), "\n";
 
-	print scalar($point->latlon), "\n";
-	sleep $time;
+
+	#print scalar($point->toString), "\n";
+	#try to create NMEA Sentence
+	#if ($point->lat > 0) {$latdir = "N";} else {$latdir="S";}
+        #if ($point->long > 0) {$londir = "E";} else {$londir="W";}
+	#$nmeaString = "GPGLL," .scalar( abs($point->lat *100)) . ",$latdir,";
+	#$nmeaString .= scalar(abs($point->long *100)) . ",$londir,";	
+	#$checksum ^= ord($nmeaString);
+	#print "\$" . $nmeaString . "*";
+	#print $checksum;
+	#print "\n";
+
+
+#	sleep $time;
 }
